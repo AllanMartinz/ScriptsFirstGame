b5e8482c3aba14d4ce4026531074f3a3409ffc01
Allan Martins <allanmartinscf6@gmail.com>
2024-08-09 19:46:50 -0300

    add trampolim
    09/08/24
    adicionado o trampolim e algumas explicacoes

---
+++ Trampoline.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+
+    //arrumar a animacao de pulo - ok
+
+    public float jumpForce;
+
+    Animator Ani;
+    private void Start()
+    {
+        Ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+       // Ani.SetBool("isjump", false);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // !nota: a collisao vai retornar ao player
+        if (collision.gameObject.tag == "Player") 
+        {
+            //ao colidir vai pegar o Rigidbody2D e add forca do tipo impulso | e a forca sera atribuida no vetor y
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            Ani.SetTrigger("isjump");
+        }
+    }
+}
